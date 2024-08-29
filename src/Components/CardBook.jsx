f842276fed555e34e9a9fa924648e832a48aef32
Thiago Silva <titusilvs91@gmail.com>
2024-08-28 22:59:24 -0300

    Estilos, animações, fluidez e experiências de usuabilidade melhoradas

--- src/Components/CardBook.jsx
+++ src/Components/CardBook.jsx
@@ -1,18 +1,46 @@
 import React from "react";
-import "/src/styles/CardBook.css"; 
+import "/src/styles/CardBook.css";
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const CardBook = ({ img, titulo, autor }) => {
+  const [isActive, setIsActive] = React.useState(false);
+  const [isHovered, setIsHovered] = React.useState(false);
+  const timeoutIdRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (isHovered) {
+      timeoutIdRef.current = setTimeout(() => {
+        setIsActive(true);
+      }, 1000);
+    } else {
+      clearTimeout(timeoutIdRef.current);
+      setIsActive(false);
+    }
+
+    return () => clearTimeout(timeoutIdRef.current);
+  }, [isHovered]);
+
   return (
-    <div className="card">
-      <div className="img" style={{ backgroundImage: `url(${img})` }}></div>
-      <div className="content">
-        <span className="title">{titulo}</span>
-        <p className="desc">
-          {autor}
-        </p>
-      </div>
-      <div className="arrow">
-        <span>&#8673;</span>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="card-wrapper"
+    >
+      <div className="card">
+        <div className="img" style={{ backgroundImage: `url(${img})` }}></div>
+        <div className={`content ${isActive ? "active" : ""}`}>
+          <span className="title">{titulo}</span>
+          <p className="desc">{autor}</p>
+          <div className="actions">
+            <button>Ler Livro</button>
+            <button>Saiba Mais</button>
+          </div>
+        </div>
+        <div className="arrow">
+          <span>
+            <TiArrowSortedDown />
+          </span>
+        </div>
       </div>
     </div>
   );
